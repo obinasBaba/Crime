27e56ed57d52e2fd1b961769afab5f1cbd512fc1
henok <henokgetachew500@gmail.com>
2020-05-11 17:12:03 +0300

    fixing lastUpdate bug

--- app/src/main/java/com/hfad/criminalintentkotlini/UI/Fragemnts/TimeStampFragment.kt
+++ app/src/main/java/com/hfad/criminalintentkotlini/UI/Fragemnts/TimeStampFragment.kt
@@ -1,17 +1,53 @@
 package com.hfad.criminalintentkotlini.UI.Fragemnts
 
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Intent
+import android.icu.util.GregorianCalendar
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
-class TimeStampFragment() : DialogFragment()
+class TimeStampFragment : DialogFragment()
 {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    companion object{
+        private const val DATE_BUNDLED_KEY = "initial date to preview"
+        const val SERIALIZED_DATE = "serialized date"
+        fun getInstance( date: Date? ) : TimeStampFragment {
+            val bundle = Bundle().apply {
+                putSerializable( DATE_BUNDLED_KEY, date ?: Date() )
+            }
 
+           return TimeStampFragment().apply {
+               arguments = bundle
+           }
+        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("RestrictedApi")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendarInstance = Calendar.getInstance()
+        calendarInstance.time = requireArguments()[ DATE_BUNDLED_KEY ] as Date
+
+        val year = calendarInstance.get( Calendar.YEAR )
+        val month = calendarInstance.get( Calendar.MONTH )
+        val day = calendarInstance.get( Calendar.DAY_OF_MONTH )
+
+        val setDateListener = DatePickerDialog.OnDateSetListener{ _ , modifiedYear, modifiedMonth, modifiedDayOfMonth ->
+            val intentToTargetFragment = Intent( ).apply {
+                val newDate = GregorianCalendar( modifiedYear, modifiedMonth, modifiedDayOfMonth ).time
+                putExtra( SERIALIZED_DATE, newDate )
+            }
+
+            targetFragment?.onActivityResult( CrimeDetailFragment.DATE_REQUEST_CODE, Activity.RESULT_OK, intentToTargetFragment )
+        }
+
+        return DatePickerDialog( requireContext(), setDateListener, year, month, day )
     }
 }
